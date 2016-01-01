ce3642a19ddd68971e79152ecc9e777b95a9076e
Andrew Denta <adenta@indiana.edu>
2016-01-01 16:26:50 -0500

    added dummy config.js
    added a file that contains the private information to run super pun game.

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports.db = {
+
+    'secret': 'MONGO_SECRET',
+    'database':  'MONGO_URI'
+
+};
+module.exports.flickrConnect = {
+  api:"FLICKR_API_KEY",
+  secret:"FLICKR_API_SECRET"
+
+}
